e27e791dfd42e1c6bcfb5aae77a77fe3d9939a18
meera-9787 <movie123frame@gmail.com>
2025-12-04 21:52:58 +0530

    Add files via upload

---
+++ factorial.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Factorial Program using loop in php</title>
+</head>
+<body>
+<form method="post">
+<input type="number" name="number" id="number">
+<input type="submit" name="submit" value="submit">
+</form>
+
+<?php
+if($_POST){
+    $fact = 1;
+    $number = $_POST['number'];
+    echo "Factorial of $number : <br><br>";
+
+    for($i = 1; $i <= $number; $i++){
+        $fact = $fact * $i;
+    }
+
+    echo $fact . "<br>";
+}
+?>
+</body>
+</html>

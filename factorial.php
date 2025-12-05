0e83be51c03e160d0045634f9781be6622c52818
meera-9787 <movie123frame@gmail.com>
2025-12-05 23:43:59 +0530

    Update factorial.php

--- factorial.php
+++ factorial.php
@@ -7,18 +7,15 @@
 <input type="number" name="number" id="number">
 <input type="submit" name="submit" value="submit">
 </form>
-
 <?php
 if($_POST){
-    $fact = 1;
-    $number = $_POST['number'];
-    echo "Factorial of $number : <br><br>";
-
-    for($i = 1; $i <= $number; $i++){
-        $fact = $fact * $i;
-    }
-
-    echo $fact . "<br>";
+$fact=1;
+$number=$_POST['number'];
+echo"Factorial of $number : <br><br>";
+for($i=1;$i<=$number;$i++){
+$fact=$fact*$i;
+}
+echo$fact."<br>";
 }
 ?>
 </body>

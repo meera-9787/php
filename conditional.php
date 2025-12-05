10dc415d2ce76972bbc6877e9f1ffd6ef0b8ec40
meera-9787 <movie123frame@gmail.com>
2025-12-05 23:41:04 +0530

    Update conditional.php

--- conditional.php
+++ conditional.php
@@ -1,37 +1,34 @@
 <?php
 $t=10;
-$temprature =60;
+$temprature=60;
 echo'<br> Demonstration of the conditional statements';
-echo '<br>';
+echo'<br>';
 echo'<br> if else statement<br>';
 echo'..................<br>';
-if($t<0)
-{
-echo $t. ' is a negative number';
+if($t<0){
+echo$t.' is a negative number';
 }
-else
-{
-echo $t. ' is a positive number<br>';
+else{
+echo$t.' is a positive number<br>';
 }
 echo'<br><br>switch statement<br>';
 echo'***************<br>';
-switch($temprature)
-{
-	case 60:
-	case 65:
-	case 70:
-	echo "nice day outside.";
-	break;
-	case 75:
-	case 78:
-	echo "ok,but a little warm,";
-	break;
-	case 80:
-	case 45:
-	case 90:
-	echo " A little warmer.";
-	break;
-	default:
-	echo "temperature outside the range this statement can handle";
+switch($temprature){
+case 60:
+case 65:
+case 70:
+echo"nice day outside.";
+break;
+case 75:
+case 78:
+echo"ok,but a little warm,";
+break;
+case 80:
+case 45:
+case 90:
+echo" A little warmer.";
+break;
+default:
+echo"temperature outside the range this statement can handle";
 }
 ?>

97d3f0d638af518c3b35e932403c1b03872b936a
meera-9787 <movie123frame@gmail.com>
2025-12-14 21:36:27 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,23 +1,19 @@
-
-
 <?php
-if(isset($_POST['file_name']))
+if(isset($_POST['file_name'])) 
 {
-
 $file=$_POST['file_name'];
 header('content-type:image/jpg');
-header('content-Disposition:attachment;filename="'.$file.'"');
+header('content-Disposition:attachment;filename="'"'.$file.'"'"');
 readfile('force_Download/'.$file);
 exit();
 }
 ?>
-
 <form action="forcc_dowload.php" method="post" name="downloadform">
 <tr>
-<td><input type="hidden" name="file_name" value="Desert.jpg"></td>
+<td><input type="text" name="file_name" value="Desert.jpg" type="hidden"></td>
 </tr>
 <tr>
 <td colspan="2" align="center">
-<input type="submit" name="button" value="download"></td>
+<input type="button" name="button" value="download"></td>
 </tr>
 </form>
